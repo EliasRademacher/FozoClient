a8aece75ab1a7f954bd7d2df9c0f9cae0a047a7d
EliasRademacher <rademace@onid.oregonstate.edu>
2016-11-11 09:08:10 -0800

    Get rid of all Spring stuff.

--- app/src/main/java/com/eli/fozoclient/MainActivity.java
+++ app/src/main/java/com/eli/fozoclient/MainActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
 import model.Person;
 
 /**
@@ -14,30 +11,16 @@ import model.Person;
  */
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void makeGETRequest(View view) {
 
-        Person person = new Person();
 
-        // The connection URL
-        final String url = "http://fozotest.appspot.com/people";
-
-        // Create a new RestTemplate instance
-        RestTemplate restTemplate = new RestTemplate();
-
-        // Add the String message converter
-        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-
-        // Make the HTTP GET request, marshaling the response to a String
-        String result = restTemplate.getForObject(url, String.class, "Android");
-
-        System.out.println(result);
     }
 
 }
