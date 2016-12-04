a85f2dc050e3ee4fa9c32da35625045e4b4bda21
EliasRademacher <rademace@onid.oregonstate.edu>
2016-12-04 12:30:09 -0800

    Add Volley GET request to viewAllPeople(). Gets BAD REQUEST because it's not including the token or userId in the header.

--- app/src/main/java/com/eli/fozoclient/ViewAllPeopleActivity.java
+++ app/src/main/java/com/eli/fozoclient/ViewAllPeopleActivity.java
@@ -4,10 +4,26 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ExpandableListView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import model.Person;
 
 public class ViewAllPeopleActivity extends ListActivity {
 
@@ -25,10 +41,11 @@ public class ViewAllPeopleActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_people);
 
+        viewAllPeople();
+
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
         setListAdapter(adapter);
 
-        viewAllPeople();
     }
 
     /* Method which will handle dynamic insertion. */
@@ -38,7 +55,64 @@ public class ViewAllPeopleActivity extends ListActivity {
 //        accountList.setAdapter();
 
 
-        listItems.add("Clicked : " + clickCounter++);
-        adapter.notifyDataSetChanged();
+
+        /* Instantiate the RequestQueue. */
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://fozo-145621.appspot.com/accounts";
+
+
+        /* Request a string response from the provided URL. */
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                url,
+                createResponseListener(),
+                createErrorListener()
+        );
+
+        queue.add(stringRequest);
     }
+
+
+    private Response.Listener<String> createResponseListener() {
+        return new Response.Listener<String>() {
+            @Override
+            public void onResponse(String responseAsString) {
+//                Response response = null;
+//                try {
+//                    response = (Response) jsonResponse.get("payload");
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+
+
+                ObjectMapper mapper = new ObjectMapper();
+
+                Response response = null;
+                try {
+                    response = mapper.readValue(responseAsString, Response.class);
+                } catch (IOException e) {
+                    System.out.println(e.getMessage() + "\n");
+                    e.printStackTrace();
+                }
+
+                System.out.println("Retrieved list of Accounts!");
+
+                listItems.add("Clicked : " + clickCounter++);
+                adapter.notifyDataSetChanged();
+
+            }
+        };
+    }
+
+    private Response.ErrorListener createErrorListener() {
+        return new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                view.setText("Error: " + error.getMessage());
+                System.out.println("Error: " + error.toString());
+                error.printStackTrace();
+            }
+        };
+    }
+
 }
