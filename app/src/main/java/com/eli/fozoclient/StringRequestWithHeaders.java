64584d34cc2ab50fb580c9f6f999912e8ba69928
EliasRademacher <rademace@onid.oregonstate.edu>
2016-12-04 23:08:55 -0800

    Add a logout button to the end of the list of accounts.

--- app/src/main/java/com/eli/fozoclient/StringRequestWithHeaders.java
+++ app/src/main/java/com/eli/fozoclient/StringRequestWithHeaders.java
@@ -26,6 +26,7 @@ public class StringRequestWithHeaders extends StringRequest {
         super(url, listener, errorListener);
     }
 
+
     public  String getToken() {
         return token;
     }
--- app/src/main/java/com/eli/fozoclient/ViewAllPeopleActivity.java
+++ app/src/main/java/com/eli/fozoclient/ViewAllPeopleActivity.java
@@ -1,8 +1,10 @@
 package com.eli.fozoclient;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import com.android.volley.Request;
@@ -192,5 +194,21 @@ public class ViewAllPeopleActivity extends ListActivity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
+    }
+
+    public void logout(View view) {
+        HeaderKeeper.getInstance().setHeaders(null);
+        HeaderKeeper.getInstance().setUserId(null);
+
+//        StringRequestWithHeaders stringRequest = new StringRequestWithHeaders();
+//        stringRequest.setUserId(null);
+//        stringRequest.setToken(null);
+//        JsonObjectRequestWithHeaders jsonRequest = new JsonObjectRequestWithHeaders();
+//        jsonRequest.setUserId(null);
+//        jsonRequest.setToken(null);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        this.startActivity(intent);
+
     }
 }
